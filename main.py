fc251108ff0c56a3755ee9e79ebe5f46b4441317
Bhavanvir Rai <bhavanvir.r@gmail.com>
2021-12-30 02:12:55 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -2,15 +2,11 @@ from pip._vendor import requests
 from datetime import datetime
 import json
 
-"""
-'date': datetime.today().strftime('%Y-%m-%d')
-"""
-
-def fetch_info(website, statistic, location):
+def fetch_info(website, statistic, location, date):
     params = {
-        'stat': statistic,
-        'loc': location,
-        'date': '2021-12-29'
+        'stat': statistic.upper(),
+        'loc': location.upper(),
+        'date': date
     }
     
     response = requests.get(website, params)
@@ -19,8 +15,13 @@ def fetch_info(website, statistic, location):
         print("An error has occured.", response)
     else:
         data = response.json()
+
+        if len(data['cases']) == 0:
+            print("If 'today' was selected, no cases have been reported yet; please try again later.")
+            exit(0)
     
     print("Province: ", location)
+    print("Date: ", date)
     if statistic == 'cases':
         for item in data['cases']:
             print("Cases: ", item['cases'])
@@ -40,26 +41,39 @@ def fetch_info(website, statistic, location):
 def main():
     url = 'https://api.opencovid.ca/timeseries'
 
-    provinces = [
-                    'alberta', 'british columbia', 'manitoba', 'new brunswick', 'newfoundland and labrador', 'northwest territories', 
-                    'nova scotia', 'nunavut', 'ontario', 'prince edward island', 'quebec', 'saskatchewan', 'yukon'
-                ]
-
     codes = ['ab', 'bc', 'mb', 'nb', 'nl', 'nt', 'ns', 'nu', 'on', 'pe', 'qc', 'sk', 'yt']            
 
     valid_stat = ['cases', 'mortality', 'recovered', 'testing', 'active']
 
     while True:
-        raw_loc = input("Enter a province or press 'q' to exit: ")
+        print("Below are valid province codes:", end="")
+        letter_codes = """
+        • Alberta: AB
+        • British Columbia: BC
+        • Manitoba: MB
+        • New Brunswick: NB
+        • Newfoundland and Labrador: NL
+        • Northwest Territories: NT
+        • Nova Scotia: NS
+        • Nunavut: NU
+        • Ontario: ON
+        • Prince Edward Island: PE
+        • Quebec: QC
+        • Saskatchewan: SK
+        • Yukon: YT
+        """
+        print(letter_codes)
+
+        raw_loc = input("Enter a two-letter province code or press 'q' to exit: ")
 
         if raw_loc in ['q', 'Q']:
             exit(0)
         else:
-            if raw_loc.lower() in provinces or raw_loc.lower() in codes:
-                print("Valid province. \n")
+            if raw_loc.lower() in codes:
+                print("Valid two-letter province code. \n")
                 break
             else:
-                print("Not a valid province, please try again. \n")
+                print("Not a valid two-letter province code, please try again. \n")
 
     while True:
         print("Below are valid statistics:", end="")
@@ -69,7 +83,7 @@ def main():
         • recovered 
         • testing 
         • active
-                """
+        """
         print(stats)
 
         raw_stat = input("Enter a inquired statistic: ")
@@ -83,7 +97,23 @@ def main():
             else:
                 print("Not a valid statistic, please try again \n")
 
-    fetch_info(url, raw_stat, raw_loc)            
+    while True:
+        raw_date = input("Please enter a valid date in YYYY-MM-DD format or enter 'today' for the current date: ")
+
+        if raw_date in ['q', 'Q']:
+            exit(0)
+        else:
+            try:
+                if raw_date.lower() == 'today':
+                    raw_date = datetime.today().strftime('%Y-%m-%d')
+                else:
+                    datetime.strptime(raw_date, '%Y-%m-%d')
+                print("Valid date format. \n")
+                break
+            except ValueError:
+                print("Specified date is not in YYYY-MM-DD format. \n")
+
+    fetch_info(url, raw_stat, raw_loc, raw_date)            
 
 if __name__ == "__main__":
     main()
